948a45b86cb3af732fded9866d81fbc671da3e4e
S3ncha4All <jonathan.ross@adesso.de>
2023-03-28 15:38:45 +0200

    Added basic TimeRecord methods.

---
+++ src/main/java/de/s3ncha4all/trm/control/TimeRecordWorker.java
@@ -0,0 +1,47 @@
+package de.s3ncha4all.trm.control;
+
+
+import de.s3ncha4all.trm.model.TaskRecord;
+import de.s3ncha4all.trm.model.TimeRange;
+import de.s3ncha4all.trm.model.TimeRecord;
+import lombok.AllArgsConstructor;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+public class TimeRecordWorker {
+
+    private TimeRecord record;
+
+    public TaskRecord createTask() {
+        TaskRecord task = new TaskRecord();
+        return task;
+    }
+
+    public void addTask(String name, TaskRecord task) {
+        record.getRecord().put(name, task);
+    }
+
+    public TimeRange createTimeRangeNow() {
+        Timestamp ts = getCurrentTimestamp();
+        TimeRange range = new TimeRange(ts);
+        return range;
+    }
+
+    public void addTimeRangeToTask(String name, TimeRange range) {
+        record.getRecord().get(name).getPastRecords().add(range);
+    }
+
+    public void closeActiveTimeRange(String name) {
+        TaskRecord task = record.getRecord().get(name);
+        TimeRange range = task.getActiveTimeRecord();
+        Timestamp ts = getCurrentTimestamp();
+        range.setEnd(ts);
+        task.getPastRecords().add(range);
+        task.setActiveTimeRecord(null);
+    }
+
+    private Timestamp getCurrentTimestamp() {
+        return new Timestamp(System.currentTimeMillis());
+    }
+}
