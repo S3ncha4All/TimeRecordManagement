3b307d91c60766401034558467fee024ea0dd9a0
S3ncha4All <jonathan.ross@adesso.de>
2023-03-29 09:35:47 +0200

    Updated Startup.

--- src/main/java/de/s3ncha4all/trm/Main.java
+++ src/main/java/de/s3ncha4all/trm/Main.java
@@ -1,12 +1,7 @@
 package de.s3ncha4all.trm;
 
 import de.s3ncha4all.trm.control.Core;
-import de.s3ncha4all.trm.control.TimeRecordLoader;
-import de.s3ncha4all.trm.control.TimeRecordWorker;
-import de.s3ncha4all.trm.model.TaskRecord;
-import de.s3ncha4all.trm.model.TimeRange;
-import de.s3ncha4all.trm.model.TimeRecord;
-import de.s3ncha4all.trm.view.TRMTrayMenu;
+
 
 /**
  * Starter.
