40512ab4567804d57f43680afd101d4f887aa699
S3ncha4All <jonathan.ross@adesso.de>
2023-03-29 10:45:17 +0200

    Reader for TimeRecord-model.

---
+++ src/main/java/de/s3ncha4all/trm/view/TimeRecordReader.java
@@ -0,0 +1,14 @@
+package de.s3ncha4all.trm.view;
+
+import de.s3ncha4all.trm.model.TimeRecord;
+import lombok.AllArgsConstructor;
+
+
+@AllArgsConstructor
+public class TimeRecordReader {
+
+    private TimeRecord record;
+
+    //get all available attributenames
+    //
+}
