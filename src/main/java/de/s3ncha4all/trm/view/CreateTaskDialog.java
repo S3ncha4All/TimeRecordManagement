ead3858a8819f74d5e7733ef76edc218ef93c8af
S3ncha4All <jonathan.ross@adesso.de>
2023-03-29 10:44:40 +0200

    Added new Task Input Dialog.

---
+++ src/main/java/de/s3ncha4all/trm/view/CreateTaskDialog.java
@@ -0,0 +1,16 @@
+package de.s3ncha4all.trm.view;
+
+import de.s3ncha4all.trm.model.TaskRecord;
+
+import javax.swing.*;
+
+public class CreateTaskDialog extends JDialog {
+
+    public CreateTaskDialog() {
+        
+    }
+
+    public TaskRecord getTask() {
+        return null;
+    }
+}
